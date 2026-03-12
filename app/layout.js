868f9545b73e27d22d76518f924688268f24261f
im24a-ademaja <ademaja@bzz.ch>
2026-03-12 10:04:19 +0100

    Änderung Font

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,7 @@ export default function RootLayout({ children }) {
         <html lang="de">
         <head>
             <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;800&display=swap" rel="stylesheet" />
+            <link href="https://fonts.googleapis.com/css2?family=Outfit:wght@300;400;600;800&display=swap" rel="stylesheet" />
         </head>
         <body>{children}</body>
         </html>
